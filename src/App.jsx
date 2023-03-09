778a6b0ddc1211ea5887aa96809055667d83335f
hossensumon071 <55638643+hossensumon071@users.noreply.github.com>
2023-03-09 10:59:42 +0600

    navbar added

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import { Routes, Route, Navigate } from "react-router-dom";
+import Footer from "./components/Footer";
+import Navbar from "./components/Navbar";
+import Chat from "./pages/Chat";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+
+const App = () => {
+  return (
+    <div className="container mx-auto">
+      <div className="upperlevel-contents min-h-screen">
+        <Navbar/>
+        <Routes>
+          <Route path="/" element={<Chat />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="*" element={<Navigate to="/" />} />
+        </Routes>
+      </div>
+      <Footer/>
+    </div>
+  );
+};
+
+export default App;
