d6db38e1ae7f5b9fe2be4c53bc3cacd5abb05f03
hossensumon071 <55638643+hossensumon071@users.noreply.github.com>
2023-03-10 08:18:52 +0600

    from control added

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,9 +1,36 @@
-import React from 'react';
+import React, { useState } from "react";
+import SectionTitle from "../components/SectionTitle";
 
 const Register = () => {
+  const [fromFields, setFromFields] = useState({
+    name: "",
+    email: "",
+    password: "",
+  });
+
+  const handleRegister = () => {
+    e.preventDefault();
+  };
   return (
-    <div>
-      register
+    <div className="register flex flex-col justify-center items-center">
+      <form onSubmit={handleRegister} className="flex flex-col gap-5">
+        <SectionTitle title={"Register..."} />
+        <div className="form-control flex flex-col gap-2">
+          <label htmlFor="name" className="cursor-pointer">
+            Name
+          </label>
+          <input
+            type="text"
+            placeholder="write your name"
+            id="name"
+            value={fromFields.name}
+            onChange={(e) => {
+              setFromFields({ ...fromFields, name: e.target.value });
+            }}
+            className="border py-3 px-5 w-[25rem] rounded outline-none focus:border-violet-500"
+          />
+        </div>
+      </form>
     </div>
   );
 };
